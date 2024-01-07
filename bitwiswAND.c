2626a75c9af4e12cd86c557b3547dfbdc06b30e6
Bisu23 <155870274+Bisu23@users.noreply.github.com>
2024-01-07 11:46:02 +0530

    Create bitwiswAND.c

---
+++ bitwiswAND.c
@@ -0,0 +1,18 @@
+//Program to find the bitwise AND of two numbers using switch statement:
+#include<stdio.h>
+int main()
+{
+int num1,num2,result;
+printf("Enter ist number:");
+scanf("%d",&num1);
+printf("Enter 2nd number:");
+scanf("%d",&num2);
+switch(0)
+{
+ case 0:
+result = num1 & num2;
+printf("%d & %d = %d", num1, num2, result);
+break;
+}
+return 0;
+}
